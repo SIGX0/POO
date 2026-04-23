f102b87f2d5b1d454faac32242f3470670515b4b
Samygarcipro <143281600+Samygarcipro@users.noreply.github.com>
2026-04-22 21:43:30 -0500

    Add files via upload
    Factory Method

---
+++ EnvioBarcoCreator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodN
+{
+    public class EnvioBarcoCreator : EnvioCreator
+    {
+        protected override IEnvio CrearEnvio()
+        {
+            return new EnvioBarco();
+        }
+    }
+}
