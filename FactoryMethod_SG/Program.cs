e05bdf271b615be33ba674ca7be97eef7797d4fc
Samygarcipro <143281600+Samygarcipro@users.noreply.github.com>
2026-04-28 08:05:15 -0500

    Add files via upload

---
+++ FactoryMethod_SG/Program.cs
@@ -0,0 +1,22 @@
+﻿using FactoryMethodN;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        EnvioCreator envioCorreo = new EnvioCorreoCreator();
+        envioCorreo.ProcesarEnvio();
+
+        EnvioCreator envioMoto = new EnvioMotoCreator();
+        envioMoto.ProcesarEnvio();
+
+        EnvioCreator envioCamion = new EnvioCamionCreator();
+        envioCamion.ProcesarEnvio();
+
+        EnvioCreator envioBarco = new EnvioBarcoCreator();
+        envioBarco.ProcesarEnvio();
+
+        EnvioCreator envioAvion = new EnvioAvionCreator();
+        envioAvion.ProcesarEnvio();
+    }
+}
